b58c0a6d049357e84711aca94c2e051dde399a74
vidarandrebo <vidarandre.bo@protonmail.com>
2024-01-16 15:45:48 +0100

    feat: read and write operation on key-value-store

--- keyvaluestorage.go
+++ keyvaluestorage.go
@@ -11,33 +11,72 @@ import (
 type KeyValueStorage map[string]map[int64]int64
 
 func NewKeyValueStorage() *KeyValueStorage {
-	data := KeyValueStorage{}
-	return &data
+	return &KeyValueStorage{}
 }
 
-// ReadValue reads and combines the values from all neighbouring nodes
+// ReadValue reads and combines the stored values from all neighbouring nodes
 func (kvs KeyValueStorage) ReadValue(key int64) (int64, error) {
-	return 0, fmt.Errorf("not implemented")
+	agg := int64(0)
+	found := false
+	for _, values := range kvs {
+		if value, exists := values[key]; exists {
+			found = true
+			agg += value
+		}
+	}
+	if found {
+		return agg, nil
+	}
+	return 0, fmt.Errorf("keyvaluestorage does not contain key %v", key)
 }
 
-// ReadValueFromNode reads the value from a single node
-func (kvs KeyValueStorage) ReadValueFromNode(addr string, key int64) (int64, error) {
-	if nodeValues, containsNode := kvs[addr]; containsNode {
+// ReadValueFromNode reads the stored value from a single node
+func (kvs KeyValueStorage) ReadValueFromNode(nodeAddr string, key int64) (int64, error) {
+	if nodeValues, containsNode := kvs[nodeAddr]; containsNode {
 		if value, containsKey := nodeValues[key]; containsKey {
 			return value, nil
 		} else {
 			return 0, fmt.Errorf("keyvaluestorage does not contain key %v", key)
 		}
 	} else {
-		return 0, fmt.Errorf("keyvaluestorage does not contain address %v", addr)
+		return 0, fmt.Errorf("keyvaluestorage does not contain address %v", nodeAddr)
 	}
 }
 
-// ReadValueExceptNode reads and combines the values from all neighbouring nodes except the input addr
-func (kvs KeyValueStorage) ReadValueExceptNode(addr string, key int64) (int64, error) {
-	return 0, fmt.Errorf("not implemented")
+// ReadValueExceptNode reads and combines the stored values from all neighbouring nodes except the input addr
+//
+// Will return (0, nil) and not (0, err) if only the excluded node has a value for the input key
+func (kvs KeyValueStorage) ReadValueExceptNode(exceptAddr string, key int64) (int64, error) {
+	agg := int64(0)
+	found := false
+	for nodeAddr, values := range kvs {
+		if value, exists := values[key]; exists {
+			found = true
+			if exceptAddr != nodeAddr {
+				agg += value
+			}
+		}
+	}
+	if found {
+		return agg, nil
+	}
+	return 0, fmt.Errorf("keyvaluestorage does not contain key %v", key)
 }
 
-func (kvs KeyValueStorage) WriteValue(addr string, key int64, value int64) error {
-	return fmt.Errorf("not implemented")
+// WriteValue writes the input value to storage
+//
+// Creates a new address key if it does not exist.
+// Overwrites existing key value pairs for a given address.
+// Returns true if value has changed.
+func (kvs KeyValueStorage) WriteValue(addr string, key int64, value int64) bool {
+	if _, containsNode := kvs[addr]; !containsNode {
+		kvs[addr] = make(map[int64]int64)
+	}
+	if storedValue, exists := kvs[addr][key]; exists {
+		if storedValue == value {
+			return false
+		}
+	}
+	kvs[addr][key] = value
+	return true
 }
--- keyvaluestorage_test.go
+++ keyvaluestorage_test.go
@@ -10,6 +10,7 @@ var keyValueStorage = KeyValueStorage{
 		1: 12,
 		2: 13,
 		3: 14,
+		5: 40,
 	},
 	"addr2": {
 		1: 15,
@@ -30,16 +31,80 @@ var keyValueStorage = KeyValueStorage{
 	},
 }
 
-func TestReadValueFromNode(t *testing.T) {
+func TestKeyValueStorage_ReadValueFromNode(t *testing.T) {
 	value, err := keyValueStorage.ReadValueFromNode("addr1", 1)
 	assert.Nil(t, err)
 	assert.Equal(t, int64(12), value)
 }
-func TestReadValueFromNodeNoAddr(t *testing.T) {
-	_, err := keyValueStorage.ReadValueFromNode("addr99", 1)
+func TestKeyValueStorage_ReadValueFromNode_NoAddr(t *testing.T) {
+	value, err := keyValueStorage.ReadValueFromNode("addr99", 1)
+	assert.Equal(t, int64(0), value)
 	assert.NotNil(t, err)
 }
-func TestReadValueFromNodeNoKey(t *testing.T) {
-	_, err := keyValueStorage.ReadValueFromNode("addr1", 99)
+func TestKeyValueStorage_ReadValueFromNode_NoKey(t *testing.T) {
+	value, err := keyValueStorage.ReadValueFromNode("addr1", 99)
+	assert.Equal(t, int64(0), value)
 	assert.NotNil(t, err)
+}
+
+func TestKeyValueStorage_ReadValue(t *testing.T) {
+	value, err := keyValueStorage.ReadValue(4)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(55), value)
+
+	value, err = keyValueStorage.ReadValue(1)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(86), value)
+}
+
+func TestKeyValueStorage_ReadValue_NoKey(t *testing.T) {
+	value, err := keyValueStorage.ReadValue(99)
+	assert.NotNil(t, err)
+	assert.Equal(t, int64(0), value)
+}
+func TestKeyValueStorage_ReadValueExceptNode(t *testing.T) {
+	value, err := keyValueStorage.ReadValueExceptNode("addr1", 4)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(55), value)
+
+	value, err = keyValueStorage.ReadValueExceptNode("addr4", 1)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(71), value)
+}
+
+func TestKeyValueStorage_ReadValueExceptNode_NoKey(t *testing.T) {
+	value, err := keyValueStorage.ReadValueExceptNode("addr1", 99)
+	assert.NotNil(t, err)
+	assert.Equal(t, int64(0), value)
+}
+
+func TestKeyValueStorage_ReadValueExceptNode_FoundZero(t *testing.T) {
+	value, err := keyValueStorage.ReadValueExceptNode("addr1", 5)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(0), value)
+}
+
+func TestKeyValueStorage_WriteValue_NoAddr(t *testing.T) {
+	testAddr := "addr55"
+	testKey := int64(1)
+	testValue := int64(10)
+	valueChanged := keyValueStorage.WriteValue(testAddr, testKey, testValue)
+	assert.Equal(t, keyValueStorage[testAddr][testKey], testValue)
+	assert.True(t, valueChanged)
+}
+func TestKeyValueStorage_WriteValue_OverWrite(t *testing.T) {
+	testAddr := "addr1"
+	testKey := int64(2)
+	testValue := int64(15)
+	valueChanged := keyValueStorage.WriteValue(testAddr, testKey, testValue)
+	assert.Equal(t, keyValueStorage[testAddr][testKey], testValue)
+	assert.True(t, valueChanged)
+}
+func TestKeyValueStorage_WriteValue_NoChange(t *testing.T) {
+	testAddr := "addr2"
+	testKey := int64(2)
+	testValue := int64(16)
+	valueChanged := keyValueStorage.WriteValue(testAddr, testKey, testValue)
+	assert.Equal(t, keyValueStorage[testAddr][testKey], testValue)
+	assert.False(t, valueChanged)
 }
