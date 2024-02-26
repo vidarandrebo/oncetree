d77b5a9848a8bc39ec94c50f8418a7a0accf1db0
vidarandrebo <vidarandre.bo@protonmail.com>
2024-02-26 19:36:45 +0100

    feat: mutex type

---
+++ mutex_test.go
@@ -0,0 +1,24 @@
+package oncetree
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRWMutex_Lock(t *testing.T) {
+	mut := NewRWMutex[uint32](32)
+	value := mut.Lock()
+	assert.Equal(t, uint32(32), *value)
+}
+
+// TestRWMutex_Unlock checks that the lock is released properly and the reference is destroyed while preserving the value
+func TestRWMutex_Unlock(t *testing.T) {
+	mut := NewRWMutex[uint32](32)
+	value := mut.Lock()
+	mut.Unlock(&value)
+	assert.Nil(t, value)
+
+	reloadedValue := mut.Lock()
+	assert.Equal(t, uint32(32), *reloadedValue)
+}
