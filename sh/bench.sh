a868e54901df0e5b5c4cf662fec23c14f5d5f13e
vidarandrebo <vidarandre.bo@protonmail.com>
2024-05-22 23:31:10 +0200

    feat: script to start replicas

---
+++ sh/bench.sh
@@ -0,0 +1,4 @@
+for x in bbchain{19..27}; do ssh $x mkdir -p logs; done
+echo bbchain19
+ssh bbchain19 "nohup ./bin/benchmarkreplica 1> /dev/null 2> /dev/null &"
+for x in bbchain{20..27}; do echo $x && ssh $x "nohup ./bin/benchmarkreplica --known-address bbchain19:8080 2>> /dev/null >> /dev/null &" ; done
---
+++ sh/stop.sh
@@ -0,0 +1,1 @@
+for x in bbchain{19..27}; do ssh $x killall benchmarkreplica; done
