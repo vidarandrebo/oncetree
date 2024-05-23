4a74283133b57831cd94f50d51c1ba9e5203911c
vidarandrebo <vidarandre.bo@protonmail.com>
2024-05-23 08:56:24 +0200

    chore: sh formatting

--- sh/clean.sh
+++ sh/clean.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-for x in bbchain{19..27}; do echo $x && ssh $x rm -r bin logs ; done
+for x in bbchain{19..27}
+do
+  echo $x
+  ssh $x rm -r bin logs
+done
--- sh/publish_bbchain.sh
+++ sh/publish_bbchain.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-for x in bbchain{19..27}; do echo $x && rsync -rP oncetree/bin $x:; done
+for x in bbchain{19..27}
+do
+  echo $x
+  rsync -rP oncetree/bin $x:
+done
